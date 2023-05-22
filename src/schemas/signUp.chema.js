fbf21c3fcf22fe1f72ee450d10c22ab662a409cd
joseedivandro <jose.edivandro.ja@outlook.com>
2023-05-22 18:00:03 -0300

    correção schema usuário

--- src/schemas/signUp.chema.js
+++ src/schemas/signUp.chema.js
@@ -1,8 +1,9 @@
-import joi from "joi"
+
+import joi from "joi";
 
 export const signUpSchema = joi.object({
-    name: joi.string().trim().invalid("").required(),
+    name: joi.string().trim().required(),
     email: joi.string().email().trim().invalid("").required(),
     password: joi.string().invalid("").required(),
     confirmPassword: joi.string().valid(joi.ref('password')).required()
-})
+});
