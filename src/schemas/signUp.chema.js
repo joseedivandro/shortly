66a655f7b39b687f1dc3cd091357641fc9a6db9e
joseedivandro <jose.edivandro.ja@outlook.com>
2023-05-22 19:08:16 -0300

    tentando corrigir o signup

--- src/schemas/signUp.chema.js
+++ src/schemas/signUp.chema.js
@@ -2,8 +2,8 @@
 import joi from "joi";
 
 export const signUpSchema = joi.object({
-    name: joi.string().trim().required(),
-    email: joi.string().email().trim().invalid("").required(),
+    name: joi.string().required(),
+    email: joi.string().email().required(),
     password: joi.string().required(),
     confirmPassword: joi.string().valid(joi.ref('password')).required()
 });
